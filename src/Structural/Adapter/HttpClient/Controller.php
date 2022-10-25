22d52f7c215a6c7053ee2e0df57e4bf33d1c8af6
Karol Kreft <karol@kreft.net.pl>
2022-10-25 12:25:13 +0200

    Structural Patterns - Adapter - PSR17 adapter example

--- src/Structural/Adapter/HttpClient/Controller.php
+++ src/Structural/Adapter/HttpClient/Controller.php
@@ -4,24 +4,17 @@ declare(strict_types=1);
 
 namespace DesignPatterns\Structural\Adapter\HttpClient;
 
-use Symfony\Component\HttpClient\HttpClient;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
+use Nyholm\Psr7\Factory\Psr17Factory;
+use Symfony\Component\HttpClient\Psr18Client;
 
 final class Controller
 {
     public function getRicky(): void
     {
-        /** @var HttpClientInterface $client */
-        $client = HttpClient::create();
-        $response = $client->request(
-            'GET',
-            'https://rickandmortyapi.com/api/character/15'
-        );
+        $client = new Psr18Client(null, new Psr17Factory());
+        $request = $client->createRequest('GET', 'https://rickandmortyapi.com/api/character/15');
+        $response = $client->sendRequest($request);
 
-        $statusCode = $response->getStatusCode();
-        $content = $response->getContent();
-//        $content = $response->toArray();
-
-        var_dump($content);
+        var_dump($response->getBody()->getContents());
     }
 }
